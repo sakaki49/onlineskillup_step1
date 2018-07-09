82d83ee4dd9c2d095798761f904ea5f4a96c755f
sakaki49 <sakakihara0409.gmail.com>
2018-07-09 17:00:28 +0900

    first commit

---
+++ post.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>POSTのサンプル</title>
+  </head>
+  <body>
+    <?php
+      //commentがPOSTされているなら
+      if(isset($_POST["comment"])){
+        //エスケープしてから表示
+	$hn = htmlspecialchars($_POST["hn"]);
+        $comment = htmlspecialchars($_POST["comment"]);
+	print("名前:${hn}\n");
+	echo nl2br("\n");
+        print("コメント:${comment}");
+	echo nl2br("\n");
+      } else {
+    ?>
+	<p>名前</p>
+        <form method="POST" action="post.php">
+         <input name="hn">
+        <p>コメントしてください。</p>
+        <form method="POST" action="post.php">
+          <input name="comment" />
+          <input type="submit" value="送信" />
+        </form>
+    <?php
+      }
+    ?>
+  </body>
+</html>
